c4ad7b0a99b022f5abd4ab74872f61703d4d5933
Szarka <75731397+BaktaiNorbert@users.noreply.github.com>
2022-05-16 19:10:13 +0200

    Sounds

--- Python projekt 5/snake.py
+++ Python projekt 5/snake.py
@@ -1,11 +1,13 @@
 #must install keyboard module pip install keyboard
+#must install playsound module pip install playsound=1.2.2
 #Python 3.10 or above required
 import keyboard
 import time 
 from math import floor
 import asyncio #több loop egyszerre tudjon futni
-import os #Konzol tisztításának módszeréhez
+from os import system, path, name #Konzol tisztításának módszeréhez
 from random import randint
+from playsound import playsound
 class Vector2: #for convinience, should contain 2 numbers.
     def __init__(self, ix, iy):
         self.x = ix
@@ -43,7 +45,7 @@ class Body:
 #Initialize
 #CREATING MAP
 heuristic = False
-if os.path.exists("./config.snake"):
+if path.exists("./config.snake"):
     f = open("config.snake","r")
     l = f.readlines()
     refresh_rate = float(l[0].rstrip().split(" ")[1])
@@ -65,25 +67,29 @@ apple = Body("O",Vector2(randint(2,screen.resolution.x),randint(1,screen.resolut
 for line in middle:
     middle_out += line
 #START GAME
-os.system('cls' if os.name == 'nt' else 'clear')#clear screen
+system('cls' if name == 'nt' else 'clear')#clear screen
 inp = 4
 last_positions = []
 pause = False
+sound_allowed = True
 def GameOver():
-        os.system('cls' if os.name == 'nt' else 'clear')
+        system('cls' if name == 'nt' else 'clear')
+        playsound("./sounds/ouch.wav", 0)
         print("\u001b[31m  .-_'''-.      ____    ,---.    ,---.    .-''-.              ,-----.    ,---.  ,---.   .-''-.  .-------.     \n '_( )_   \   .'  __ `. |    \  /    |  .'_ _   \           .'  .-,  '.  |   /  |   | .'_ _   \ |  _ _   \    \n|(_ o _)|  ' /   '  \  \|  ,  \/  ,  | / ( ` )   '         / ,-.|  \ _ \ |  |   |  .'/ ( ` )   '| ( ' )  |    \n. (_,_)/___| |___|  /  ||  |\_   /|  |. (_ o _)  |        ;  \  '_ /  | :|  | _ |  |. (_ o _)  ||(_ o _) /    \n|  |  .-----.   _.-`   ||  _( )_/ |  ||  (_,_)___|        |  _`,/ \ _/  ||  _( )_  ||  (_,_)___|| (_,_).' __  \n'  \  '-   .'.'   _    || (_ o _) |  |'  \   .---.        : (  '\_/ \   ;\ (_ o._) /'  \   .---.|  |\ \  |  | \n \  `-'`   | |  _( )_  ||  (_,_)  |  | \  `-'    /         \ `\"/  \  ) /  \ (_,_) /  \  `-'    /|  | \ `'   / \n  \        / \ (_ o _) /|  |      |  |  \       /           '. \_/``\".'    \     /    \       / |  |  \    /  \n   `'-...-'   '.(_,_).' '--'      '--'   `'-..-'              '-----'       `---`      `'-..-'  ''-'   `'-'   \n \033[0m")
         time.sleep(2)
         quit()
+async def PlaySound():
+    playsound("./sounds/click.wav")
 async def GetInput(): # w - 1, s - 2, a - 3, d - 4, p - quit(), p - pause
     global player
     global pause
     global inp
     while True:
         await asyncio.sleep(0.005)
-        if keyboard.is_pressed("w") and player.rotation != 2:inp = 1
-        elif keyboard.is_pressed("s")and player.rotation != 1:inp = 2
-        elif keyboard.is_pressed("a")and player.rotation != 4:inp = 3
-        elif keyboard.is_pressed("d")and player.rotation != 3:inp = 4
+        if keyboard.is_pressed("w") and player.rotation != 2 and inp != 1:inp = 1; await PlaySound()
+        elif keyboard.is_pressed("s")and player.rotation != 1 and inp != 2:inp = 2; await PlaySound()
+        elif keyboard.is_pressed("a")and player.rotation != 4 and inp != 3:inp = 3; await PlaySound()
+        elif keyboard.is_pressed("d")and player.rotation != 3 and inp != 4:inp = 4; await PlaySound()
         elif keyboard.is_pressed("q"):quit() #Kilépés
         elif keyboard.is_pressed("p"):pause = not pause #Megállítás átmenetileg
 async def RunGame():
@@ -100,6 +106,7 @@ async def RunGame():
                     GameOver()
             if player.position.x == apple.position.x and player.position.y == apple.position.y:
                 player.points += 1
+                playsound("./sounds/Score.wav",0)
                 apple = Body("O",Vector2(randint(2,screen.resolution.x),randint(1,screen.resolution.y-2)))
                 for i in last_positions:
                     if apple.position.x == i.x and apple.position.y == i.y:
@@ -131,19 +138,18 @@ async def RunGame():
                 middle_out += line
             screen.display([top,middle_out,bottom])
             first_frame = False
-            print("Length: ",len(last_positions),"Position:",player.position.x,player.position.y)
+            print("Length: \033[33m",len(last_positions),"\033[00m Position:\033[33m",player.position.x,player.position.y,"\033[00m\033[0m")
 async def Main():
     print(f"\033[92m           _____                    _____                    _____                    _____                    _____          \n          /\    \                  /\    \                  /\    \                  /\    \                  /\    \         \n         /::\    \                /::\____\                /::\    \                /::\____\                /::\    \        \n        /::::\    \              /::::|   |               /::::\    \              /:::/    /               /::::\    \       \n       /::::::\    \            /:::::|   |              /::::::\    \            /:::/    /               /::::::\    \      \n      /:::/\:::\    \          /::::::|   |             /:::/\:::\    \          /:::/    /               /:::/\:::\    \     \n     /:::/__\:::\    \        /:::/|::|   |            /:::/__\:::\    \        /:::/____/               /:::/__\:::\    \    \n     \:::\   \:::\    \      /:::/ |::|   |           /::::\   \:::\    \      /::::\    \              /::::\   \:::\    \   \n   ___\:::\   \:::\    \    /:::/  |::|   | _____    /::::::\   \:::\    \    /::::::\____\________    /::::::\   \:::\    \  \n  /\   \:::\   \:::\    \  /:::/   |::|   |/\    \  /:::/\:::\   \:::\    \  /:::/\:::::::::::\    \  /:::/\:::\   \:::\    \ \n /::\   \:::\   \:::\____\/:: /    |::|   /::\____\/:::/  \:::\   \:::\____\/:::/  |:::::::::::\____\/:::/__\:::\   \:::\____\ \n \:::\   \:::\   \::/    /\::/    /|::|  /:::/    /\::/    \:::\  /:::/    /\::/   |::|~~~|~~~~~     \:::\   \:::\   \::/    /\n  \:::\   \:::\   \/____/  \/____/ |::| /:::/    /  \/____/ \:::\/:::/    /  \/____|::|   |           \:::\   \:::\   \/____/ \n   \:::\   \:::\    \              |::|/:::/    /            \::::::/    /         |::|   |            \:::\   \:::\    \     \n    \:::\   \:::\____\             |::::::/    /              \::::/    /          |::|   |             \:::\   \:::\____\    \n     \:::\  /:::/    /             |:::::/    /               /:::/    /           |::|   |              \:::\   \::/        \n      \:::\/:::/    /              |::::/    /               /:::/    /            |::|   |               \:::\   \/____/     \n       \::::::/    /               /:::/    /               /:::/    /             |::|   |                \:::\    \         \n        \::::/    /               /:::/    /               /:::/    /              \::|   |                 \:::\____\        \n         \::/    /                \::/    /                \::/    /                \:|   |                  \::/    /        \n          \/____/                  \/____/                  \/____/                  \|___|                   \/____/         \n                                                                                                                              \033[0m")
     s = " "*123
     for i in range(63):
         s = s[:64-i] + "_"*i*2 + s[64 + i+1::]
         print("\033[F\033[92m",s)
-        #print("\033[F\033[92m","_"*(i+1), end="\n")
         time.sleep(0.006)
     print("\033[F"*23 + "           _____                    _____                    _____                    _____                    _____          \n          /\    \                  /\    \                  /\    \                  /\    \                  /\    \         \n         /oo\    \                /oo\____\                /oo\    \                /oo\____\                /oo\    \        \n        /oooo\    \              /oooo|   |               /oooo\    \              /ooo/    /               /oooo\    \       \n       /oooooo\    \            /ooooo|   |              /oooooo\    \            /ooo/    /               /oooooo\    \      \n      /ooo/\ooo\    \          /oooooo|   |             /ooo/\ooo\    \          /ooo/    /               /ooo/\ooo\    \     \n     /ooo/__\ooo\    \        /ooo/|oo|   |            /ooo/__\ooo\    \        /ooo/____/               /ooo/__\ooo\    \    \n     \ooo\   \ooo\    \      /ooo/ |oo|   |           /oooo\   \ooo\    \      /oooo\    \              /oooo\   \ooo\    \   \n   ___\ooo\   \ooo\    \    /ooo/  |oo|   | _____    /oooooo\   \ooo\    \    /oooooo\____\________    /oooooo\   \ooo\    \  \n  /\   \ooo\   \ooo\    \  /ooo/   |oo|   |/\    \  /ooo/\ooo\   \ooo\    \  /ooo/\ooooooooooo\    \  /ooo/\ooo\   \ooo\    \ \n /oo\   \ooo\   \ooo\____\/oo /    |oo|   /oo\____\/ooo/  \ooo\   \ooo\____\/ooo/  |ooooooooooo\____\/ooo/__\ooo\   \ooo\____\ \n \ooo\   \ooo\   \oo/    /\oo/    /|oo|  /ooo/    /\oo/    \ooo\  /ooo/    /\oo/   |oo|~~~|~~~~~     \ooo\   \ooo\   \oo/    /        \n  \ooo\   \ooo\   \/____/  \/____/ |oo| /ooo/    /  \/____/ \ooo\/ooo/    /  \/____|oo|   |           \ooo\   \ooo\   \/____/ \n   \ooo\   \ooo\    \              |oo|/ooo/    /            \oooooo/    /         |oo|   |            \ooo\   \ooo\    \     \n    \ooo\   \ooo\____\             |oooooo/    /              \oooo/    /          |oo|   |             \ooo\   \ooo\____\    \n     \ooo\  /ooo/    /             |ooooo/    /               /ooo/    /           |oo|   |              \ooo\   \oo/    /    \n      \ooo\/ooo/    /              |oooo/    /               /ooo/    /            |oo|   |               \ooo\   \/____/     \n       \oooooo/    /               /ooo/    /               /ooo/    /             |oo|   |                \ooo\    \         \n        \oooo/    /               /ooo/    /               /ooo/    /              \oo|   |                 \ooo\____\        \n         \oo/    /                \oo/    /                \oo/    /                \o|   |                  \oo/    /        \n          \/____/                  \/____/                  \/____/                  \|___|                   \/____/         \n ")
     print(" "*50,"Made by: Cyberfox Version 1.1")
-    time.sleep(1)
+    playsound("./sounds/blOOOP.mp3")
     print("\033[0m",end="\n")
-    os.system('cls' if os.name == 'nt' else 'clear')
+    system('cls' if name == 'nt' else 'clear')
     await asyncio.gather(GetInput(),RunGame())
 asyncio.run(Main())
